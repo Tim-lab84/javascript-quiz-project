e9fcb71da4befb65f0043ee335196e4b18a4a0da
Tim-lab84 <Timothycalvinwilliams@gmail.com>
2025-01-13 17:39:30 +0100

    Solved Day1

--- src/question.js
+++ src/question.js
@@ -2,6 +2,18 @@ class Question {
     // YOUR CODE HERE:
     //
     // 1. constructor (text, choices, answer, difficulty)
-
-    // 2. shuffleChoices()
+        constructor(text, choices, answer, difficulty){
+          this.text = text;
+          this.choices = choices;
+          this.answer = answer;
+          this.difficulty = difficulty;
+        }
+      
+    // 2. shuffleChoices() => how to shuffle an array
+    shuffleChoices(){
+        for ( let i = this.choices.length;i>0;i--){
+            const j = Math.floor(Math.random()*(i+1));
+            [this.choices[i],this.choices[j]] = [this.choices[j], this.choices[i]];
+        }
+    }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,51 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    //make sure to stay in bounds
+    if (this.currentQuestionIndex < this.questions.length - 1) {
+      this.currentQuestionIndex++;
+    } else {
+      console.log("You have finished the quiz");
+    }
+  }
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
+    }
+  }
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    if (answer) this.correctAnswers++;
+  }
+  // 6. hasEnded()
+  hasEnded() {
+    return this.currentQuestionIndex >= this.questions.length;
+    /** 
+        if(this.currentQuestionIndex  < this.questions.length) {
+            return false;
+        }else
+        {
+            return true;
+        }
+        */
+  }
 }
